597075746cbff04c5cdb66560e2c68376b83c5d5
bala-amypo <bala@amypo.in>
2025-12-17 06:48:49 +0000

    User commit

--- src/main/java/com/example/demo/entity/Studententity.java
+++ src/main/java/com/example/demo/entity/Studententity.java
@@ -2,45 +2,54 @@ package com.example.demo.entity;
 
 import java.time.LocalDate;
 
-public class Studententity {
-      private  String name;
-        @id
-        @Generatedvalue(strategy=GenerationType.IDENTITY)
-        private Long  id;
-        private LocalDate dOB;
-        private Float cGPA;
-        public String getName() {
-            return name;
-        }
-        public void setName(String name) {
-           this. name = name;
-        }
-        public int getid() {
-            return roll;
-        }
-        public void setid(int roll) {
-            this.roll = roll;
-        }
-        public LocalDate getDOB() {
-            return dOB;
-        }
-        public void setDOB(LocalDate dOB) {
-            this.dOB = dOB;
-        }
-        public Float getCGPA() {
-            return cGPA;
-        }
-        public void setCGPA(Float cGPA) {
-           this. cGPA = cGPA;
-        }
-        public Studententity(String name, int id, LocalDate dOB, Float cGPA) {
-           this. name = name;
-            this.id = id; 
-           this. dOB = dOB;
-            this.cGPA = cGPA;
-        }
-        public Studententity() {
-        }
-      
-    }
 
+public class Studententity {
+    @Id
+    @Generatedvalue(strategy=GenerationType.IDENTITY);
+    private Long id;
+    private  String name;
+    @Column(name=unique)
+    private String email;
+    private String password;
+    private String role;
+    public Studententity(Long id, String name, String email, String password, String role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getRole() {
+        return role;
+    }
+    public Studententity() {
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    }
