8de127c581afd4a22e12c4fc115da8bb9ff13521
bala-amypo <bala@amypo.in>
2025-12-17 08:25:40 +0000

    User commit

--- src/main/java/com/example/demo/entity/Studententity.java
+++ src/main/java/com/example/demo/entity/Studententity.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
-
+@Table(name="Student")
 public class Studententity {
     @Id
     @Generatedvalue(strategy=GenerationType.IDENTITY);
