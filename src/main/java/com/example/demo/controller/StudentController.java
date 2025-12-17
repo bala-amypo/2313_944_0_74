57b63c74f4e24392f96f787dd7ff000347393a00
bala-amypo <bala@amypo.in>
2025-12-17 05:13:48 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,37 @@
+package com.example.demo.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Entity.Studententity;
+import com.example.demo.Services.Studentservices;
+
+@RestController
+public class Studentcontroller {
+    @Autowired
+    Studentservices src;
+    @PostMapping("/post")
+    public Studententity postdata(@RequestBody Studententity st) {
+        return src.savedata(st);
+    }
+    @GetMapping("/get")
+    public List<Studententity> getdata(){
+        return src.retdata();
+    }
+
+    @GetMapping("/getid/{id}")
+    public Studententity getIdValue(@PathVariable int id){
+        return src.retIdvalue(id);
+    }
+
+
+}
--- src/main/java/com/example/demo/repository/StudentRepo.java
+++ src/main/java/com/example/demo/repository/StudentRepo.java
@@ -1,7 +1,8 @@
 package com.example.demo.repository;
 
 import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.re
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.Student;
 
 @Repository
 public interface  StuentRepo extends  JpaRepository<Student,Long> {
